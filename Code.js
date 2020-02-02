1947e63687d9a4aba00907c6737bec118f0d7f43
MbagasPradana <54340164+bagas050201@users.noreply.github.com>
2020-02-02 19:10:14 +0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,5 @@
+  function play(){
+       var audio = document.getElementById("song");
+       audio.play();
+                 }
+alert ('tekan "Song" untuk memulai lagu kebangsaan Indonesia Raya')
